cb451488463cf44bf8ab7d2afbc48f13e923140b
nagarajanprince <nagarajanprince@gmail.com>
2019-02-22 09:40:22 -0800

    react basic files build

---
+++ precache-manifest.b32e852d9edd752102f322354160f23e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "f8808a972f5d357487cb",
+    "url": "/static/js/main.f8808a97.chunk.js"
+  },
+  {
+    "revision": "ba1ca5ba3f9c1e3c91ad",
+    "url": "/static/js/2.ba1ca5ba.chunk.js"
+  },
+  {
+    "revision": "f8808a972f5d357487cb",
+    "url": "/static/css/main.2a3c326f.chunk.css"
+  },
+  {
+    "revision": "425f839d829696f141d85ceefea5b75b",
+    "url": "/index.html"
+  }
+];
